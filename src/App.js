55331a956890f8ffe2068ef8089f98931bfde2e6
Gobezar <vadimkiselevv@yandex.ru>
2023-01-16 00:41:21 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.scss';
+import { useState } from 'react';
+
 
 function App() {
+
+  const [count, setCount] = useState(5)
+
+  const plus = () => {
+    setCount (count +1)
+  }
+
+  const minus = () => {
+    setCount (count -1)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <div>
+          <h2>Счетчик:</h2>
+          <h1>{count}</h1>
+          <button onClick={minus} className="minus">- Минус</button>
+          <button onClick={plus} className="plus">Плюс +</button>
+        </div>
+      </div>
   );
 }
 
